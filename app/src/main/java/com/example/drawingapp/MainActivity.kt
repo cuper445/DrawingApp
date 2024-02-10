7725b72be1afc3ffbed82bf5a689e54ff0c6ce8a
cuper445 <98920866+cuper445@users.noreply.github.com>
2024-02-10 16:35:47 -0700

    Created some classes and set some stuff up

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import com.example.drawingapp.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
